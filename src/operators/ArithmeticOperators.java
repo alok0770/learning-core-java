5555e6e0c0d86028b5e851f1707df42129747d44
Alok Rana <alokrana0770@gmail.com>
2026-08-05 17:35:57 +0530

    Add arithmetic operators examples

---
+++ src/operators/ArithmeticOperators.java
@@ -0,0 +1,41 @@
+package operators;
+
+public class ArithmeticOperators {
+    public static void main (String [] args){
+
+        int firstNumber = 63 ;
+        int secondNumber = 56 ;
+
+        //this variable store the addition result
+        int additionResult = firstNumber + secondNumber ;
+
+        //printing the addition result
+        System.out.println("Addition result : " + additionResult);
+
+        // this variable stores the subtraction result
+        int subtractionResult = firstNumber - secondNumber ;
+
+        // printing the subtraction result
+        System.out.println("Subtraction Result : " + subtractionResult);
+
+        // this variable stores the multiplication result
+        int multiplicationResult = firstNumber * secondNumber ;
+
+        // printing the multiplication result
+        System.out.println("Multiplication Result : " + multiplicationResult);
+
+        // this variable stores the division result
+        int divisionResult = firstNumber / secondNumber ;
+
+        // printing the division result
+        System.out.println("Division Result : " + divisionResult);
+
+        // this variable stores the modulus result
+        int modulusResult = firstNumber % secondNumber ;
+
+        // printing the modulus result
+        System.out.println("Modulus Result : " + modulusResult);
+
+    }
+
+}
