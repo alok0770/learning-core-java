9aa4e1660c54a64be13c89ed33f8dd7c6d3869fd
Alok Rana <alokrana0770@gmail.com>
2026-08-11 15:50:41 +0530

    2d arrays add new dail pad array

--- src/arrays/TwoDimensionalArray.java
+++ src/arrays/TwoDimensionalArray.java
@@ -1,4 +1,60 @@
 package arrays;
 
 public class TwoDimensionalArray {
-}
+
+        public static void main(String[] args) {
+
+            // 2D String array containing different grocery items
+            String[][] groceries = {
+                    {"Apple", "Orange", "Banana"},
+                    {"Potato", "Carrot", "Tomato"},
+                    {"Chicken", "Mutton", "Fish"}
+            };
+
+            // Updating specific elements using row and column indexes
+            groceries[1][1] = "Onion";
+            groceries[2][0] = "ButterChicken";
+
+            // Printing the 2D array using nested enhanced for loops
+            System.out.println("================================");
+            System.out.println("        GROCERY ITEMS");
+            System.out.println("================================");
+
+            for (String[] item : groceries) {
+
+                for (String items : item) {
+                    System.out.print(items + "   ");
+                }
+
+                System.out.println();
+            }
+
+            // --------------------------------------------
+
+            System.out.println("\n================================");
+            System.out.println("        DIAL PAD");
+            System.out.println("================================");
+
+            // 2D character array representing a telephone dial pad
+            char[][] telephone = {
+                    {'1', '2', '3'},
+                    {'4', '5', '6'},
+                    {'7', '8', '9'},
+                    {'*', '0', '#'}
+            };
+
+            // Printing the telephone dial pad
+            // using nested enhanced for loops
+            for (char[] phone : telephone) {
+
+                for (char calls : phone) {
+                    System.out.print(calls + "    ");
+                }
+
+                System.out.println();
+            }
+
+            System.out.println("================================");
+        }
+    }
+
